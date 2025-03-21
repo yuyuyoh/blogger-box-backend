d3e8c31e645d34217ef8b5412b0fa90dc5e9f10c
yuxuan chen <yuyuyoh@dsip322f.windows.dauphine.fr>
2025-03-21 10:56:31 +0100

    Ajout des fichiers un par un

---
+++ src/main/java/com/dauphine/blogger_box_backend/BloggerBoxBackendApplication.java
@@ -0,0 +1,13 @@
+package com.dauphine.blogger_box_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloggerBoxBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BloggerBoxBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dauphine/blogger_box_backend/BloggerBoxBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dauphine.blogger_box_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloggerBoxBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
